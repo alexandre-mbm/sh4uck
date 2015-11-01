199b5074846dc1729dbbd08dec2225af7f05cb82
Alexandre Magno <alexandre.mbm@gmail.com>
2015-11-01 02:14:12 -0300

    sh4uck.sh inicial

---
+++ sh4uck.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#
+# Inicialmente este programa executará tarefas separamente. Exemplo:
+#
+#   $ sh4suck server      # instala software e configurações de rede
+#
+
+# TODO: verificar execução como root
+
+function inexistent() {
+    echo "alvo \"$1\" não implementado"
+}
+
+case "$1" in
+    internet)
+        # TODO: Firefox 28 com atalho e xpi de tradução para português
+        # TODO: Java 6.31 e criar link simbólico na pasta do firefox
+        # TODO: Default proxy settings firefox
+        # TODO: Extensões para todos usuários (tradução firefox para pt-BR)
+        # TODO: Proxy firefox para todos os usuários
+        # TODO: Google Chrome
+        inexistent "internet"
+        ;;
+    server) 
+        # TODO: X2go
+        # TODO: SSH
+        # TODO: Scripts de rede
+        # TODO: ocsinventoryagent
+        # TODO: likewiseopen-5
+        # TODO: Cliente Owncloud
+        inexistent "server"
+        ;;
+    xorg)
+        # TODO: MyUnity
+        # TODO: Configurações padronizadas de ícones do unity
+        # TODO: Temas
+        # TODO: Adicionar papéis de parede
+        # TODO: xfce4
+        inexistent "xorg"
+        ;;
+    office)
+        # TODO: LibreOffice 5
+        # TODO: Inkscape
+        # TODO: Gimp
+        inexistent "office"
+        ;;  
+    hardware) 
+        # TODO: Adicionar todas as impressoras do esquadrão
+        # TODO: Driver da impressora Samsung 3310
+        inexistent "hardware"
+        ;;
+    virtual)
+        # TODO: Virtualbox
+        # TODO: Wine
+        inexistent "virtual"
+        ;;
+    boot)
+        # TODO: grub-customizer ‒ instalar
+        # TODO: grub-customizer ‒ configurar
+        # TODO: boot-repair
+        inexistent "boot"
+        ;;
+    apt)
+        # TODO: apt.conf
+        # TODO: Gerenciador de pacotes Synaptics
+        inexistent "apt"
+        ;;
+    cron)
+        # TODO: Desligamento automático às 19h
+        inexistent "cron"
+        ;;
+    fix)
+        # TODO: Resolver o problema da build da UCK não poder retomar a customização
+        inexistent "fix"
+        ;;
+    *)
+        # Ajuda
+        echo "alvos possíveis: internet server xorg office hardware virtual boot apt cron fix"
+        ;;
+esac
+
