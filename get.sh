d28ad8ab974acd71274775d1242daa6eb1640cf9
Alexandre Magno <alexandre.mbm@gmail.com>
2015-11-02 01:42:25 -0300

    ~/sh4uck-backup

--- get.sh
+++ get.sh
@@ -2,11 +2,19 @@
 
 cd ~/
 mkdir /tmp/sh4uck
-wget -c https://github.com/alexandre-mbm/sh4uck/archive/master.tar.gz -P /tmp/sh4uck
-tar -xzvf /tmp/sh4uck/master.tar.gz 
+wget -c https://github.com/alexandre-mbm/sh4uck/archive/master.tar.gz -P /tmp/sh4uck -q
+tar -xzf /tmp/sh4uck/master.tar.gz
+
+mkdir sh4uck-backup
+mv sh4uck sh4uck-backup/$(date +%Y-%m-%d.%Hh%Mm%Ss)
 mv sh4uck-master sh4uck
 cd sh4uck/
-#export PATH=$PATH:$(pwd)
 
-echo "Você instalou "$(pwd)
-echo "Estando neste diretório, pode comandar \"sudo ./sh4uck\""
+echo
+echo "Parabéns, você instalou o sh4uck!"
+echo
+echo "Para usá-lo, faça:"
+echo
+echo "  $ cd "$(pwd)
+echo "  $ sudo ./sh4uck"
+echo
